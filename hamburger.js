e6501e21de68af9007e7caff4309f987a263e82c
Folkert Hoogenraad <folkerthoogenraad@hotmail.nl>
2018-11-16 10:20:01 +0100

    Added hamburger menu and stuff

---
+++ hamburger.js
@@ -0,0 +1,22 @@
+var menuOpen = false;
+
+window.addEventListener('resize', () => setMenu(false));
+
+function toggleMenu(){
+    setMenu(!menuOpen);
+}
+
+function setMenu(menu){
+    var d = document.getElementById("navigator");
+    
+    d.style.transition = "transform 0.5s";
+    
+    menuOpen = menu;
+
+    if(menuOpen){
+        d.style.transform = "translateX(100%)";
+    }else{
+        d.style.transform = "translateX(0%)";
+    }
+
+}
